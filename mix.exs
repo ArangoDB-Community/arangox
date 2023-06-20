94e66bf4e72d77d1c4810f35240c3a7db2ec9ad9
suazithustra <33040041+suazithustra@users.noreply.github.com>
2023-06-20 17:24:26 +0400

    inc proj ver

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Arangox.MixProject do
   use Mix.Project
 
-  @version "0.5.4"
+  @version "0.5.5"
   @description """
   ArangoDB 3.4+ driver for Elixir with connection pooling, support for \
   VelocyStream, active failover, transactions and streamed cursors.
